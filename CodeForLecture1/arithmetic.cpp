b41628ca7ac28d4b11c9fb28a63cd7dd93a74907
Reinn90 <kevin.reyes1990@hotmail.com>
2025-04-17 23:43:19 +1000

    compiled first cpp arch

--- CodeForLecture1/arithmetic.cpp
+++ CodeForLecture1/arithmetic.cpp
@@ -1,20 +1,17 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-using namespace std;
 
-int main()
-{
-    const double Pi = 3.14159;
-    double radius, volume;
+int main() {
+  const double Pi = 3.14159;
+  double radius, volume;
 
-    cout << "Please enter the radius of a sphere:";
-    cin >> radius;
+  std::cout << "Please enter the radius of a sphere:";
+  std::cin >> radius;
 
-    volume = 4.0 / 3.0 * Pi * pow(radius, 3.0);
-    cout << "The volume of the sphere is: \n"
-         << volume << "\n";
+  volume = 4.0 / 3.0 * Pi * pow(radius, 3.0);
+  std::cout << "The volume of the sphere is: \n" << volume << "\n";
 
-    system("pause");
-    return 0;
+  system("pause");
+  return 0;
 }
