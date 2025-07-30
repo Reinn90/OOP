4587ef519cbf0972dd484b0c63eb8d1721ffc041
Kevin Reyes <kevin.reyes1990@hotmail.com>
2025-07-30 23:06:09 +1000

    TuteRand_v1_working

--- AcadTutorial/TuteRand.cpp
+++ AcadTutorial/TuteRand.cpp
@@ -1,11 +1,14 @@
 // OOP Tutorial Student callout randomiser
 // Author: Kevin Reyes
 
+#include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
 int main() {
+    srand(time(0));
+
     cout << "How many students are attending the tutorial? \n";
     int attendees;
 
@@ -16,10 +19,17 @@ int main() {
     }
 
     cout << "\n\tWelcome to Object Oriented Programming in C++ (Tutorial)\n";
-    cout << "\t\t\tAll " << attendees << " of you.\n";
+    cout << "\t\tAll " << attendees << " of you. Let's have some fun coding.\n";
+    cout << "\n\t\t   *No Vibe-Coding allowed here*\n";
 
     for (int i = 0; i < 70; i++) {
         cout << "#";
     }
+
+    int num;
+    num = rand() % attendees + 1;
+
+    cout << endl;
+    cout << "\nIt's student " << num << "'s turn.";
     return 0;
 }
