b5caf40f4a76a43d237cbd6bf36b019fd37434c3
Kevin Reyes <kevin.reyes1990@hotmail.com>
2025-09-15 22:06:32 +1000

    finished 4.2

---
+++ AcadTutorial/Prac4_Week5n6/OneDarray.h
@@ -0,0 +1,81 @@
+/*
+ * OneDArray.h
+ *
+ * Created on 21/8/25
+ * Author : Kevin Reyes
+ *
+ *
+ */
+
+#ifndef OneDArray_H_
+#define OneDArray_H_
+
+const int DECLARED_SIZE = 10;
+
+class OneDArray {
+    private:
+        int arr[DECLARED_SIZE];
+        int numberUsed;
+        int target;
+
+    public:
+        OneDArray() {};
+        void fillArray(int[], int, int &);
+        int search(const int[], int, int);
+        int run();
+};
+
+void OneDArray::fillArray(int a[], int size, int &numberUsed) {
+    cout << "Enter up to " << size << " nonnegative integers.\n"
+         << "Mark the end of the list with a negative number.\n";
+
+    int next, index = 0;
+    cin >> next;
+    while ((next >= 0) && (index < size)) {
+        a[index] = next;
+        index++;
+        cin >> next;
+    }
+
+    numberUsed = index;
+}
+
+int OneDArray::search(const int a[], int numberUsed, int target) {
+    int index = 0;
+    bool found = false;
+
+    // Add your code here for Task 3.1
+    for (int i = 0; i < numberUsed; i++) {
+
+        if (a[i] == target && !found) {
+            found = true;
+            index = i;
+        }
+    }
+
+    if (found)
+        return index;
+    else
+        return -1;
+}
+
+int OneDArray::run() {
+
+    int listSize, target;
+
+    fillArray(arr, DECLARED_SIZE, listSize);
+
+    cout << "Enter a number to search for: ";
+    cin >> target;
+
+    int result = search(arr, listSize, target);
+    if (result == -1)
+        cout << target << " is not on the list.\n";
+    else
+        cout << target << " is stored in array position " << result << endl
+             << "(Remember: The first position is 0.)\n";
+
+    return 0;
+}
+
+#endif /* OneDArray_H_ */
---
+++ AcadTutorial/Prac4_Week5n6/Task4_2.cpp
@@ -0,0 +1,12 @@
+// Searches a partially filled array of nonnegative integers.
+#include <iostream>
+
+using namespace std;
+
+#include "OneDarray.h"
+
+int main() {
+    OneDArray o;
+    o.run();
+    return 0;
+}
